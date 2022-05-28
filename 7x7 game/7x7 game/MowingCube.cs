c92db4e51d966d6ba85fca8348023877a7fe8925
abalint49 <abalint49@gmail.com>
2022-05-28 08:43:34 +0200

    .

---
+++ 7x7 game/7x7 game/MowingCube.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7x7_game
+{
+    internal class MowingCube
+    {
+        Color[] sides = new Color[5];
+        byte red = 2;
+
+        public Color GetColor(int side)
+        {
+            return sides[side];
+        }
+
+        public MowingCube(Color[] sides)
+        {
+            this.sides = sides;
+        }
+
+        public string[,] MoveUp(string[,] map, byte[] palayerLocation, out string succes, out byte[] palayerLocation1)
+        {
+            if (palayerLocation[0] == 7 && palayerLocation[1] == 3)
+            {
+                succes = "Finish";
+                palayerLocation1 = palayerLocation;
+                return map;
+            }
+            if(map[palayerLocation[0]-1,palayerLocation[1]] != " H " && sides[0] != Color.piros)
+            {
+                switch (red)
+                {
+                    case 2:
+                        {
+                            red = 0;
+                            sides[0] = Color.piros;
+                            sides[2] = Color.kék;
+                            break;
+                        }
+                    case 4:
+                        {
+                            red = 2;
+                            sides[2] = Color.piros;
+                            sides[4] = Color.kék;
+                            break;
+                        }
+                }
+                map[palayerLocation[0], palayerLocation[1]] = "   ";
+                map[palayerLocation[0] - 1, palayerLocation[1]] = " # ";
+                palayerLocation1 = new byte[] { (byte)((int)palayerLocation[0] - 1), palayerLocation[1] };
+                succes = "succes";
+                return map;
+            }
+            succes = "failed";
+            palayerLocation1 = palayerLocation;
+            return map;
+        }
+        public string[,] MoveDown(string[,] map, byte[] palayerLocation, out string succes, out byte[] palayerLocation1)
+        {
+            if (palayerLocation[0] == 7 && palayerLocation[1] == 3)
+            {
+                succes = "Finish";
+                palayerLocation1 = palayerLocation;
+                return map;
+            }
+            if (map[palayerLocation[0] + 1, palayerLocation[1]] != " H " && sides[4] != Color.piros)
+            {
+                switch (red)
+                {
+                    case 0:
+                        {
+                            red = 2;
+                            sides[2] = Color.piros;
+                            sides[0] = Color.kék;
+                            break;
+                        }
+                    case 2:
+                        {
+                            red = 4;
+                            sides[4] = Color.piros;
+                            sides[2] = Color.kék;
+                            break;
+                        }
+                }
+                map[palayerLocation[0], palayerLocation[1]] = "   ";
+                map[palayerLocation[0] + 1, palayerLocation[1]] = " # ";
+                palayerLocation1 = new byte[] { (byte)((int)palayerLocation[0] + 1), palayerLocation[1] };
+                succes = "succes";
+                return map;
+            }
+            succes = "failed";
+            palayerLocation1 = palayerLocation;
+            return map;
+        }
+        public string[,] MoveLeft(string[,] map, byte[] palayerLocation, out string succes, out byte[] palayerLocation1)
+        {
+            if (palayerLocation[0] == 7 && palayerLocation[1] == 3)
+            {
+                succes = "Finish";
+                palayerLocation1 = palayerLocation;
+                return map;
+            }
+            if (map[palayerLocation[0], palayerLocation[1] - 1] != " H " && sides[1] != Color.piros)
+            {
+                switch (red)
+                {
+                    case 2:
+                        {
+                            red = 1;
+                            sides[1] = Color.piros;
+                            sides[2] = Color.kék;
+                            break;
+                        }
+                    case 3:
+                        {
+
+                            red = 2;
+                            sides[2] = Color.piros;
+                            sides[3] = Color.kék;
+                            break;
+                        }
+                }
+                map[palayerLocation[0], palayerLocation[1]] = "   ";
+                map[palayerLocation[0], palayerLocation[1] - 1] = " # ";
+                palayerLocation1 = new byte[] { palayerLocation[0], (byte)((int)palayerLocation[1] - 1) };
+                succes = "succes";
+                return map;
+            }
+            palayerLocation1 = palayerLocation;
+            succes = "failed";
+            return map;
+        }
+        public string[,] MoveRight(string[,] map, byte[] palayerLocation, out string succes, out byte[] palayerLocation1)
+        {
+            if (palayerLocation[0] == 7 && palayerLocation[1] == 3)
+            {
+                succes = "Finish";
+                palayerLocation1 = palayerLocation;
+                return map;
+            }
+            if (map[palayerLocation[0], palayerLocation[1] + 1] != " H " && sides[3] != Color.piros)
+            {
+                switch (red)
+                {
+                    case 1:
+                        {
+
+                            red = 2;
+                            sides[2] = Color.piros;
+                            sides[1] = Color.kék;
+                            break;
+                        }
+                    case 2:
+                        {
+
+                            red = 3;
+                            sides[3] = Color.piros;
+                            sides[2] = Color.kék;
+                            break;
+                        }
+                }
+                map[palayerLocation[0], palayerLocation[1]] = "   ";
+                map[palayerLocation[0], palayerLocation[1] + 1] = " # ";
+                palayerLocation1 = new byte[] { palayerLocation[0], (byte)(palayerLocation[1] + 1) };
+                succes = "succes";
+                return map;
+            }
+            palayerLocation1 = palayerLocation;
+            succes = "failed";
+            return map;
+        }
+
+        public bool TryMoveUp(string[,] map, byte[] palayerLocation)
+        {
+            if (map[palayerLocation[0] - 1, palayerLocation[1]] != " H " && sides[0] != Color.piros)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool TryMoveDown(string[,] map, byte[] palayerLocation)
+        {
+            if (map[palayerLocation[0] + 1, palayerLocation[1]] != " H " && sides[4] != Color.piros)
+            {
+                
+                return true;
+            }
+            return false;
+        }
+        public bool TryMoveLeft(string[,] map, byte[] palayerLocation)
+        {
+            if (map[palayerLocation[0], palayerLocation[1] - 1] != " H " && sides[1] != Color.piros)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool TryMoveRight(string[,] map, byte[] palayerLocation)
+        {
+            if (map[palayerLocation[0], palayerLocation[1] + 1] != " H " && sides[3] != Color.piros)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+
+
+    public enum Color { kék, piros}
+}
---
+++ 7x7 game/7x7 game/Program.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _7x7_game
+{
+    internal class Program
+    {
+        static Random rnd = new Random();
+
+
+        static string[,] startMap = { { " H "," H ", " H ", " H ", " H ", " H ", " H ", " H "," H " },
+                                      { " H ","   ", " H ", "   ", "   ", "   ", " H ", "   "," H " },
+                                      { " H ","   ", "   ", "   ", "   ", "   ", " # ", "   "," H " },
+                                      { " H "," H ", "   ", "   ", " H ", "   ", " H ", "   "," H " },
+                                      { " H ","   ", "   ", " H ", "   ", "   ", "   ", "   "," H " },
+                                      { " H ","   ", "   ", "   ", "   ", "   ", " H ", "   "," H " },
+                                      { " H ","   ", "   ", " H ", "   ", " H ", "   ", "   "," H " },
+                                      { " H ","   ", " H ", " C ", "   ", "   ", "   ", "   "," H " },
+                                      { " H "," H ", " H ", " H ", " H ", " H ", " H ", " H "," H " }};
+
+        static byte[] startPlayerLocation = { 2, 6 };
+
+
+        static string[,] map = startMap;
+
+        static byte[] playerLocation = startPlayerLocation;
+
+        static MowingCube player = new MowingCube(new Color[] { Color.kék, Color.kék, Color.piros, Color.kék, Color.kék });
+
+        static int WaitTime =500;
+        static void Main(string[] args)
+        {
+            string[,] map = startMap;
+
+            byte[] playerLocation = startPlayerLocation;
+
+            RefressTabe();
+
+            //Winner();
+
+            //ProbaHiba(WaitTime);
+            //FejlestettProbaHiba(WaitTime);
+
+            //RestartosProbaHiba(1,500);
+            //FejlestettRestartosProbaHiba(50,500);
+
+            EgyLépcsősMálységiKeresés(10);
+
+            Console.ReadKey();
+        }
+
+        static void RefressTabe()
+        {
+            Console.Clear();
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Console.Write(map[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n\n");
+            Console.WriteLine(String.Format($"        {player.GetColor(0)}"));
+            Console.WriteLine(String.Format($"    {player.GetColor(1)} {player.GetColor(2)} {player.GetColor(3)}"));
+            Console.WriteLine(String.Format($"        {player.GetColor(4)}"));
+        }
+
+        static public string UserMoveUp()
+        {
+            string succes = "";
+            byte[] palayerLocation2 = playerLocation;
+            Console.Clear();
+            map = player.MoveUp(map,playerLocation, out succes, out palayerLocation2);
+            playerLocation = palayerLocation2;
+            RefressTabe();
+            return succes;
+        }
+        static public string UserMoveDown()
+        {
+            string succes = "";
+            byte[] playerLocation2 = playerLocation;
+            Console.Clear();
+            map = player.MoveDown(map, playerLocation, out succes, out playerLocation2);
+            playerLocation = playerLocation2;
+            RefressTabe();
+            return succes;
+        }
+        static public string UserMoveLeft()
+        {
+            string succes = "";
+            byte[] playerLocation2 = playerLocation;
+            Console.Clear();
+            map = player.MoveLeft(map, playerLocation, out succes, out playerLocation2);
+            playerLocation = playerLocation2;
+            RefressTabe();
+            return succes;
+        }
+        static public string UserMoveRight()
+        {
+            string succes = "";
+            byte[] playerLocation2 = playerLocation;
+            Console.Clear();
+            map = player.MoveRight(map, playerLocation, out succes, out playerLocation2);
+            playerLocation = playerLocation2;
+            RefressTabe();
+            return succes;
+        }
+
+        static public void Winner()
+        {
+            Thread.Sleep(WaitTime);
+           UserMoveRight();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Thread.Sleep(WaitTime);
+                UserMoveDown();
+            }
+
+            Thread.Sleep(WaitTime);
+            UserMoveLeft();
+
+            Thread.Sleep(WaitTime);
+            UserMoveDown();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Thread.Sleep(WaitTime);
+                UserMoveLeft();
+            }
+
+            Console.WriteLine("Finish");
+        }
+
+        static public void ProbaHiba(int wait)
+        {
+            string succes = "";
+
+            while (succes != "failed")
+            {
+                int nextStep = rnd.Next(0, 3);
+
+                switch (nextStep)
+                {
+                    case 0:
+                        succes = UserMoveUp();
+                        break;
+                    case 1:
+                        succes = UserMoveDown();
+                        break;
+                    case 2:
+                        succes = UserMoveRight();
+                        break;
+                    case 3:
+                        
+                        succes = UserMoveLeft();
+                        break;
+                }
+                Thread.Sleep(wait);
+            }
+        }
+        static public void FejlestettProbaHiba(int wait)
+        {
+            string succes = "";
+            int lastStep = 9;
+
+            while (succes != "failed")
+            {
+                int nextStep = rnd.Next(0, 3);
+
+                while (nextStep == lastStep)
+                {
+                    nextStep = rnd.Next(0, 3);
+                }
+
+                switch (nextStep)
+                {
+                    case 0:
+                        succes = UserMoveUp();
+                        lastStep = 1;
+                        break;
+                    case 1:
+                        succes = UserMoveDown();
+                        lastStep = 0;
+                        break;
+                    case 2:
+                        succes = UserMoveRight();
+                        lastStep = 3;
+                        break;
+                    case 3:
+                        succes = UserMoveLeft();
+                        lastStep = 2;
+                        break;
+                }
+                Thread.Sleep(wait);
+            }
+        }
+
+        static public void RestartosProbaHiba(int waitBetweenSteps, int waitBetweenStarts)
+        {
+            while(playerLocation[0] != 7 && playerLocation[1] != 3)
+            {
+                playerLocation = new byte[] { 2, 6 };
+                map = new string[,] { { " H "," H ", " H ", " H ", " H ", " H ", " H ", " H "," H " },
+                                      { " H ","   ", " H ", "   ", "   ", "   ", " H ", "   "," H " },
+                                      { " H ","   ", "   ", "   ", "   ", "   ", " # ", "   "," H " },
+                                      { " H "," H ", "   ", "   ", " H ", "   ", " H ", "   "," H " },
+                                      { " H ","   ", "   ", " H ", "   ", "   ", "   ", "   "," H " },
+                                      { " H ","   ", "   ", "   ", "   ", "   ", " H ", "   "," H " },
+                                      { " H ","   ", "   ", " H ", "   ", " H ", "   ", "   "," H " },
+                                      { " H ","   ", " H ", " C ", "   ", "   ", "   ", "   "," H " },
+                                      { " H "," H ", " H ", " H ", " H ", " H ", " H ", " H "," H " }
+                };
+                player = new MowingCube(new Color[] { Color.kék, Color.kék, Color.piros, Color.kék, Color.kék });
+
+                ProbaHiba(waitBetweenSteps);
+
+                Thread.Sleep(waitBetweenStarts);
+            }
+        }
+        static public void FejlestettRestartosProbaHiba(int waitBetweenSteps, int waitBetweenStarts)
+        {
+            while (playerLocation[0] != 7 && playerLocation[1] != 3)
+            {
+                playerLocation = new byte[] { 2, 6 };
+                map = new string[,] { { " H "," H ", " H ", " H ", " H ", " H ", " H ", " H "," H " },
+                                      { " H ","   ", " H ", "   ", "   ", "   ", " H ", "   "," H " },
+                                      { " H ","   ", "   ", "   ", "   ", "   ", " # ", "   "," H " },
+                                      { " H "," H ", "   ", "   ", " H ", "   ", " H ", "   "," H " },
+                                      { " H ","   ", "   ", " H ", "   ", "   ", "   ", "   "," H " },
+                                      { " H ","   ", "   ", "   ", "   ", "   ", " H ", "   "," H " },
+                                      { " H ","   ", "   ", " H ", "   ", " H ", "   ", "   "," H " },
+                                      { " H ","   ", " H ", " C ", "   ", "   ", "   ", "   "," H " },
+                                      { " H "," H ", " H ", " H ", " H ", " H ", " H ", " H "," H " }
+                };
+                player = new MowingCube(new Color[] { Color.kék, Color.kék, Color.piros, Color.kék, Color.kék });
+
+                FejlestettProbaHiba(waitBetweenSteps);
+
+                Thread.Sleep(waitBetweenStarts);
+            }
+        }
+
+        static public void EgyLépcsősMálységiKeresés(int wait)
+        {
+            string succes = "";
+
+            int steps = 0;
+
+            while (succes != "failed")
+            {
+                bool up = player.TryMoveUp(map,playerLocation);
+                bool down = player.TryMoveDown(map, playerLocation);
+                bool left = player.TryMoveLeft(map, playerLocation);
+                bool right = player.TryMoveRight(map, playerLocation);
+
+                List<int> possiblestep = new List<int>();
+
+                if (up)
+                {
+                    possiblestep.Add(0);
+                }
+                if (down)
+                {
+                    possiblestep.Add(1);
+                }
+                if (right)
+                {
+                    possiblestep.Add(2);
+                }
+                if (left)
+                {
+                    possiblestep.Add(3);
+                }
+
+                int nextStep = rnd.Next(0, possiblestep.Count);
+
+                switch (possiblestep[nextStep])
+                {
+                    case 0:
+                        succes = UserMoveUp();
+                        break;
+                    case 1:
+                        succes = UserMoveDown();
+                        break;
+                    case 2:
+                        succes = UserMoveRight();
+                        break;
+                    case 3:
+                        succes = UserMoveLeft();
+                        break;
+                }
+
+                if (succes == "Finish")
+                {
+                    Console.WriteLine("Lépések száma: " + steps);
+                    break;
+                }
+
+                steps++;
+
+                Thread.Sleep(wait);
+            }
+        }
+
+    }
+}
